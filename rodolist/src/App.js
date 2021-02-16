72ad8f3b091cd515094b5de0717061cc4c35a87a
toswm128 <chominsu10@gmail.com>
2021-02-16 23:56:58 +0900

    [ADD] Show background for Input.value

--- rodolist/src/App.js
+++ rodolist/src/App.js
@@ -1,6 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
+class App extends React.Component {
+  state = {
+    query: ""
+  };
+  render() {
+    return (
+      <Container query={this.state.query}>
+        <Input
+          placeholder="테마를 입력하세요"
+          onKeyPress={this.handleInputKeyPress}
+        ></Input>
+      </Container>
+    );
+  }
+
+  handleInputKeyPress = event => {
+    if (event.key === "Enter") {
+      this.setState({
+        query: event.target.value
+      });
+      event.target.value = "";
+    }
+  };
+}
+
+const Input = styled.input`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 190px;
+  height: 33px;
+  padding: 3px;
+  background: transparent;
+  outline: none;
+  border: none;
+  font-size: 22px;
+  color: white;
+`;
+
 const Container = styled.div`
   position: absolute;
   top: 0;
@@ -13,14 +52,9 @@ const Container = styled.div`
       rgba(20, 20, 20, 0.7) 70%,
       rgba(20, 20, 20, 1) 
     ),
-    url(https://source.unsplash.com/random/1920x1080);
+    url(https://source.unsplash.com/random/1920x1080?${props => props.query});
   background-size: cover;
 `;
 
-class App extends React.Component {
-  render() {
-    return <Container></Container>;
-  }
-}
 
 export default App;
