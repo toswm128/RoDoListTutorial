1059847786cacfbc04aab38871892114c0cbfd62
toswm128 <chominsu10@gmail.com>
2021-02-21 00:07:33 +0900

    [UPDATE] optimization

--- rodolist/src/compornt/ToDoList/TodoRow.js
+++ rodolist/src/compornt/ToDoList/TodoRow.js
@@ -1,11 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-const TodoRow = ({ index, todo, handleClickRemove }) => (
-  <Container>
-    <Text onClick={() => handleClickRemove(index)}>{todo}</Text>
-  </Container>
-);
+class TodoRow extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.todo === nextProps.todo) {
+      return false;
+    }
+    return true;
+  }
+  render() {
+    const { index, todo, handleClickRemove } = this.props;
+    console.log(`${todo} 컴포넌트 렌더링`);
+    return (
+      <Container>
+        <Text onClick={() => handleClickRemove(index)}>{todo}</Text>
+      </Container>
+    );
+  }
+}
 
 const Container = styled.div`
   margin: 13px 0;
